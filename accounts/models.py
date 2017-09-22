ee4f2bb5a77ba532c212acec949907ee5c5a15c2
Duncan Betts <dunc@nbetts.com>
2017-09-22 19:56:49 +0100

    Custom passwordless auth backend + custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import uuid
 
+from django.contrib import auth
 from django.db import models
 
-# Create your models here.
+auth.signals.user_logged_in.disconnect(auth.models.update_last_login)
+
+
+class User(models.Model):
+    email = models.EmailField(primary_key=True)
+    REQUIRED_FIELDS = []  # type: list
+    USERNAME_FIELD = 'email'
+    is_anonymous = False
+    is_authenticated = True
+
+
+class Token(models.Model):
+    email = models.EmailField()
+    uid = models.CharField(default=uuid.uuid4, max_length=40)
